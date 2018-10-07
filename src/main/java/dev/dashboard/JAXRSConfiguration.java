dd938f7bd88e389fc0b77b3f4fd571705d6435da
Ivelin <Ivelin@DESKTOP-C78RK4U>
2018-10-07 19:17:46 +0300

    fixed pom / fixed rest end points

--- src/main/java/dev/dashboard/JAXRSConfiguration.java
+++ src/main/java/dev/dashboard/JAXRSConfiguration.java
@@ -5,7 +5,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
  
-@ApplicationPath("dev.dashboard.resources")
+@ApplicationPath("/")
 public class JAXRSConfiguration extends Application {
 
 }
