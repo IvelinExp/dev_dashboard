c107aab946362d2130f639e4141abfc3316b2cf8
LionIn <p1f.hbk@gmail.com>
2018-10-11 18:40:38 +0300

    the missing 2 classes - and it should work now!

---
+++ src/main/java/dev/dashboard/resources/UserStoryService.java
@@ -0,0 +1,60 @@
+package dev.dashboard.resources;
+
+import java.util.List;
+
+import dev.dashboard.dao.UserStoryDao;
+import dev.dashboard.entities.Story;
+
+public class UserStoryService {
+
+	
+	
+	private static UserStoryDao userStoryDao;
+
+	public UserStoryService() {
+		userStoryDao = new UserStoryDao();
+	}
+
+	public void persist(Story entity) {
+		userStoryDao.openCurrentSessionwithTransaction();
+		userStoryDao.persist(entity);
+		userStoryDao.closeCurrentSessionwithTransaction();
+	}
+
+	public void update(Story entity) {
+		userStoryDao.openCurrentSessionwithTransaction();
+		userStoryDao.update(entity);
+		userStoryDao.closeCurrentSessionwithTransaction();
+	}
+
+	public Story findById(String id) {
+		userStoryDao.openCurrentSession();
+		Story story = userStoryDao.findById(id);
+		userStoryDao.closeCurrentSession();
+		return story;
+	}
+
+	public void delete(String id) {
+		userStoryDao.openCurrentSessionwithTransaction();
+		Story story = userStoryDao.findById(id);
+		userStoryDao.delete(story);
+		userStoryDao.closeCurrentSessionwithTransaction();
+	}
+
+	public List<Story> findAll() {
+		userStoryDao.openCurrentSession();
+		List<Story> stories = userStoryDao.findAll();
+		userStoryDao.closeCurrentSession();
+		return stories;
+	}
+
+	public void deleteAll() {
+		userStoryDao.openCurrentSessionwithTransaction();
+		userStoryDao.deleteAll();
+		userStoryDao.closeCurrentSessionwithTransaction();
+	}
+
+	public UserStoryDao userStoryDao() {
+		return userStoryDao;
+	}
+}
